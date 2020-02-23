961189175e9b9a0c038b7b7580011949629365d6
Edvinas Pranka <epranka@gmail.com>
2020-02-23 14:02:08 +0200

    :tada: Initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [decimalIsValid, setDecimalIsValid] = useState(true);
+  const [binaryValue, setBinaryValue] = useState("");
+  const [decimalValue, setDecimalValue] = useState("");
+
+  const formatValue = value => {
+    return value.replace(/\s+/g, "");
+  };
+
+  const handleDecimalValueChange = e => {
+    let value = String(e.target.value);
+    if (!value) {
+      setDecimalValue("");
+      setDecimalIsValid(true);
+    } else {
+      setDecimalIsValid(/^([0-9]+)$/.test(value));
+      setDecimalValue(formatValue(value));
+    }
+  };
+
+  const calculateBinary = decimalNumber => {
+    return decimalNumber
+      .toString(2)
+      .replace(/(\d{4})/g, "$1 ")
+      .trim();
+  };
+
+  useEffect(() => {
+    if (decimalIsValid && decimalValue) {
+      const decimalNumber = parseInt(decimalValue.replace(/[^0-9]/g, ""));
+      const binaryValue = calculateBinary(decimalNumber);
+      setBinaryValue(binaryValue);
+    } else {
+      setBinaryValue("");
+    }
+  }, [decimalValue, decimalIsValid]);
+
+  return (
+    <div id="dec2bin">
+      <h1 id="header">Dec2Bin</h1>
+      <input
+        id="decimal"
+        className={decimalIsValid ? "" : "invalid"}
+        type="text"
+        placeholder="1234"
+        value={decimalValue}
+        onChange={handleDecimalValueChange}
+      />
+      <div id="binary">{binaryValue}</div>
+    </div>
+  );
+}
+
+export default App;
